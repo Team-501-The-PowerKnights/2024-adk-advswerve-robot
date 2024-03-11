880d033b2b913b1e8fecd6fe2418c425de707c4e
kingofl337 <kingofl337@gmail.com>
2024-03-11 16:44:41 -0400

    Added TalonFX Configs for Launcher

--- src/main/java/frc/robot/subsystems/Launcher.java
+++ src/main/java/frc/robot/subsystems/Launcher.java
@@ -1,8 +1,14 @@
 package frc.robot.subsystems;
 
-import static frc.robot.Constants.LauncherConstants.*;
+import static frc.robot.Constants.LauncherConstants.kLauncherLeft;
+import static frc.robot.Constants.LauncherConstants.kLauncherRight;
+import static frc.robot.Constants.LauncherConstants.kLauncherSpeed;
 
+import com.ctre.phoenix6.BaseStatusSignal;
+import com.ctre.phoenix6.StatusCode;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.TalonFX;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -43,6 +49,7 @@ public class Launcher extends SubsystemBase {
   // Motors - Speed Controls
   private TalonFX launcherLeft;
   private TalonFX launcherRight;
+  TalonFXConfiguration configFX;
 
   // used when launcher is in auto mode
   private double launcherSpeedAuto;
@@ -57,10 +64,48 @@ public class Launcher extends SubsystemBase {
 
     // Construct Motors
     launcherLeft = new TalonFX(kLauncherLeft);
+    launcherRight = new TalonFX(kLauncherRight);
+
+    // configure motors
     // One must be inverted
     launcherLeft.setInverted(true);
+    launcherRight.setInverted(false);
 
-    launcherRight = new TalonFX(kLauncherRight);
+    BaseStatusSignal.setUpdateFrequencyForAll(10,
+        launcherLeft.getPosition(),
+        launcherLeft.getVelocity(),
+        launcherLeft.getMotorVoltage());
+
+    // Only Send updates we ask for defining a specific signal update rate.
+    launcherLeft.optimizeBusUtilization();
+    launcherRight.optimizeBusUtilization();
+
+    // TODO: Enable Voltage Compensation for Launcher
+
+    // Configrue and share between Motors
+    configFX = new TalonFXConfiguration();
+
+    configFX.Voltage.PeakForwardVoltage = 11;
+    configFX.Voltage.PeakReverseVoltage = -11;
+
+    configFX.TorqueCurrent.PeakForwardTorqueCurrent = 40;
+    configFX.TorqueCurrent.PeakReverseTorqueCurrent = -40;
+
+    // Apply Motor Configs
+    StatusCode statusL = StatusCode.StatusCodeNotInitialized;
+    StatusCode statusR = StatusCode.StatusCodeNotInitialized;
+    for (int i = 0; i < 5; ++i) {
+      statusL = launcherLeft.getConfigurator().apply(configFX);
+      statusR = launcherRight.getConfigurator().apply(configFX);
+      if (statusL.isOK() && statusR.isOK())
+        break;
+    }
+    if (!statusL.isOK()) {
+      System.out.println("Could not apply configs, to Left Launcher error code: " + statusL.toString());
+    }
+    if (!statusR.isOK()) {
+      System.out.println("Could not apply configs, to Right Launcher error code: " + statusR.toString());
+    }
 
     // TODO: Impliment Automatic Speed Control
     launcherSpeedAuto = kLauncherSpeed;
@@ -78,6 +123,7 @@ public class Launcher extends SubsystemBase {
   public void setLauncherSpeedOL(double speed) {
     launcherLeft.set(-speed);
     launcherRight.set(-speed);
+
   }
 
   // Sets the speed of the lead motor to 0
@@ -87,7 +133,8 @@ public class Launcher extends SubsystemBase {
     launcherRight.set(0);
   }
 
-  // Use this command will command the Launcher to Do Something and goes to idle when button
+  // Use this command will command the Launcher to Do Something and goes to idle
+  // when button
   // released
   public Command setTask(Task task) {
     return this.startEnd(
@@ -99,7 +146,7 @@ public class Launcher extends SubsystemBase {
         });
   }
 
-  //runs when no commands are active
+  // runs when no commands are active
   public Command defaultCommand() {
     return this.run(
         () -> {
